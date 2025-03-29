1f6a2c287640b381bb4cfb277e9b274761823e57
Mikhail Klimov <74006803+MihelKlimm@users.noreply.github.com>
2025-03-29 15:24:30 +0600

    Update specdata_functions.R

--- specdata_functions.R
+++ specdata_functions.R
@@ -36,3 +36,65 @@ pollutantmean <- function(source, pollutantname, ids)
         mean(pollutant_values_all, na.rm = TRUE)
 }
 source <- "D:/Coursera/specdata"
+
+omplete <- function(directory, ids)
+  ##   INPUT
+  ## directory      = string with data source where files can be found
+  ## ids            = vector with ids representing monitor locations
+  
+  ##   DESCRIPTION
+  ## Function finds for each location the number of complete observations.
+  
+  ##   OUTPUT
+  ## Data frame with columns id, nobs
+  
+##   Created 19-02-2019 by Fraukje Coopmans
+{
+        ## Initialize data frame containing each id and corresponding observation #
+        output <- as.data.frame(matrix(ncol = 2, nrow = length(ids)))      
+        names(output) <- c("id", "nobs")
+        
+        for (i in 1:length(ids)) 
+        {
+                ## Create full datafile name and read data      
+                datafile <- paste(directory, "/", stri_pad_left(ids[i], 3, 0), ".csv", sep = "")
+                data <- read.csv(datafile)
+        
+                ## For each location id (i), find the number of complete observations
+                output[i,] <- c(ids[i], sum(complete.cases(data)))
+        }
+        ## Return output
+        output
+}
+
+corr <- function(directory, threshold = 0)
+  ##   INPUT
+  ## directory      = string with data source where files can be found
+  ## threshold      = number of complete cases a location must have to be    ##                  included
+  
+  ##   DESCRIPTION
+  ## Function goed through all files within the directory and finds those    ## locations that meet the threshold, and for these the correlation        ## between sulfate and nitrate levels is calculated.
+  
+  ##   OUTPUT
+  ## Numberic vector of correlations
+  
+  ##   Created 04-03-2019 by Fraukje Coopmans
+{
+  monitor_cor <- vector()
+  
+  all_files = list.files(directory)
+  ## For each file in the directory
+  for (i in all_files) {
+    # Read file and find number of complete cases
+    data <- read.csv(file.path(directory, i))
+    completes_vector <- complete.cases(data)
+    
+    ## If the threshold is met, calculate correlation
+    if (sum(completes_vector) >= threshold) {
+      monitor_cor <- c(monitor_cor, cor(data$nitrate[completes_vector], data$sulfate[completes_vector]))
+    }
+  }
+  
+  ## Return output
+  monitor_cor
+}
